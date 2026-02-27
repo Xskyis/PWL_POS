50dc3d2fea2d16be0ab474f22ecd9b8454fd4cbd
Xskyis <nabilafgareza12@gmail.com>
2026-02-28 06:37:19 +0700

    Relationship

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\LevelModel;
 
 class UserModel extends Model
 {
@@ -13,4 +14,8 @@ class UserModel extends Model
     protected $primaryKey = 'user_id';
 
     protected $fillable = ['level_id', 'username', 'nama', 'password'];
+
+    public function level() {
+        return $this->belongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
